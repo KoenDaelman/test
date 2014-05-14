9eca7f2ffa064cc2a6c4a48b1c5cb6eede936782
KoenDaelman <koendaelman@gmail.com>
2014-05-14 11:06:35 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,8 +1,1 @@
-public void invoke_touch(float x, float y, float pressure, int id) {
-    FieldPacker touch_fp = new FieldPacker(16);
-    touch_fp.addF32(x);
-    touch_fp.addF32(y);
-    touch_fp.addF32(pressure);
-    touch_fp.addI32(id);
-    invoke(mExportFuncIdx_touch, touch_fp);
-}
+uint32_t unsignedInteger = 1;
