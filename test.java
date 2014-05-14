4f4d8b65caab81b049e4f5af4a59ece88ebdc108
KoenDaelman <koendaelman@gmail.com>
2014-05-14 11:05:46 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,9 +1,8 @@
-void touch(float x, float y, float pressure, int id) {
-    if (id >= 10) {
-        return;
-    }
-
-    touchPos[id].x = x;
-    touchPos[id].y = y;
-    touchPressure[id] = pressure;
+public void invoke_touch(float x, float y, float pressure, int id) {
+    FieldPacker touch_fp = new FieldPacker(16);
+    touch_fp.addF32(x);
+    touch_fp.addF32(y);
+    touch_fp.addF32(pressure);
+    touch_fp.addI32(id);
+    invoke(mExportFuncIdx_touch, touch_fp);
 }
