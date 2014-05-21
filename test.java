ab40319238b7a75094caab06fefcedd52a8bf056
KoenDaelman <koendaelman@gmail.com>
2014-05-21 17:12:15 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,5 @@
-ScriptC_invert invert = new ScriptC_invert(mRenderScript);
+renderscript.target=18
+renderscript.support.mode=true
+sdk.buildtools=18.1.0
+
+import android.support.v8.renderscript.*;
