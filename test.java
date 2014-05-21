2ecb2fd3472cf5b297d6b74afab211f38ccf9190
KoenDaelman <koendaelman@gmail.com>
2014-05-21 17:00:36 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,7 @@
-ll
+uchar4 __attribute__((kernel)) invert(uchar4 in, uint32_t x, uint32_t y) {
+  uchar4 out = in;
+  out.r = 255 - in.r;
+  out.g = 255 - in.g;
+  out.b = 255 - in.b;
+  return out;
+}
